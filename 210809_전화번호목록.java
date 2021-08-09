d179c3be4291604df73365b2bfa24a41a1b34d6b
seop1284-kr <seop1284@naver.com>
2021-08-09 13:36:17 +0900

    프로그래머스 42577

---
+++ 210809_전화번호목록.java
@@ -0,0 +1,46 @@
+package 전화번호목록_42577;
+
+import java.util.HashMap;
+
+
+public class Solution {
+//    public boolean solution2(String[] phone_book) {
+//        boolean answer = true;
+//        HashMap<String, Integer> hm = new HashMap<String, Integer>();
+//        for (String s : phone_book) {
+//        	for (String str : hm.keySet()) {
+//        		if (str.length() < s.length()) {
+//        			if (s.startsWith(str)) {
+//        				return false;
+//        			}
+//        		} else {
+//        			if (str.startsWith(s)) {
+//        				return false;
+//        			}
+//        		}
+//        	}
+//
+//    		hm.put(s, 1);
+//        }
+//        
+//        return answer;
+//    }
+    
+//    public boolean solution(String[] phone_book) {
+//    	boolean answer = true;
+//        for (int i = 0; i < phone_book.length; i++) {
+//        	for (int j = i + 1; j < phone_book.length; j++) {
+//        		if (phone_book[i].startsWith(phone_book[j])) return false;
+//        		if (phone_book[j].startsWith(phone_book[i])) return false;
+//        	}
+//        }
+//    	return answer;
+//    }
+	
+    public boolean solution(String[] phone_book) {
+    	boolean answer = true;
+        
+    	return answer;
+    }
+    
+}
